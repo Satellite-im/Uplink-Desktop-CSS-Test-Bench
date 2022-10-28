59542526f8c368beba501a1c84f3148181f8117e
Stuart Woodbury <stuartwoodbury@protonmail.ch>
2022-10-28 14:21:03 -0400

    refactor: allow for multiple pages
    use index.html as a directory of test pages

--- server.py
+++ server.py
@@ -1,11 +1,23 @@
 from bottle import route, run, static_file, template
 
-@route('/static/styles.css')
-def styles():
-	return static_file("styles.css", root='./static')
+@route('/static/chats-sidebar.css')
+def chats_sidebar_styles():
+	return static_file("chats-sidebar.css", root='./static')
+
+@route('/static/files-sidebar.css')
+def files_sidebar_styles():
+	return static_file("files-sidebar.css", root='./static')
 
 @route('/')
 def hello():
 	return template('index.html')
+
+@route('/chats-sidebar')
+def files_sidebar():
+	return template('views/chats-sidebar.html')
+
+@route('/files-sidebar')
+def files_sidebar():
+	return template('views/files-sidebar.html')
 
 run(host='localhost', port=8080, debug=True)

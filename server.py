5096c6f9877be012788e553cf567b7805864825e
Stuart Woodbury <stuartwoodbury@protonmail.ch>
2022-11-02 17:00:15 -0400

    added pages to test favorites

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 from bottle import route, run, static_file, template
 
+@route('/static/tools.css')
+def chats_sidebar_styles():
+	return static_file("tools.css", root='./static')
+
 @route('/static/chats-sidebar.css')
 def chats_sidebar_styles():
 	return static_file("chats-sidebar.css", root='./static')
@@ -7,6 +11,14 @@ def chats_sidebar_styles():
 @route('/static/files-sidebar.css')
 def files_sidebar_styles():
 	return static_file("files-sidebar.css", root='./static')
+
+@route('/static/favorites.css')
+def files_sidebar_styles():
+	return static_file("favorites.css", root='./static')
+
+@route('/static/add-favorites.css')
+def files_sidebar_styles():
+	return static_file("add-favorites.css", root='./static')
 
 @route('/')
 def hello():
@@ -19,5 +31,13 @@ def files_sidebar():
 @route('/files-sidebar')
 def files_sidebar():
 	return template('views/files-sidebar.html')
+
+@route('/favorites')
+def files_sidebar():
+	return template('views/favorites.html')
+
+@route('/add-favorites')
+def files_sidebar():
+	return template('views/add-favorites.html')
 
 run(host='localhost', port=8080, debug=True)

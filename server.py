7f2b760716c36e9e13da32d8e6e18eb291310a72
Stuart Woodbury <stuartwoodbury@protonmail.ch>
2022-11-21 09:30:21 -0500

    feat: receive video from websocket and play in browser
    work in progress

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from bottle import route, run, static_file, template
 
 @route('/static/tools.css')
-def chats_sidebar_styles():
+def tools_styles():
 	return static_file("tools.css", root='./static')
 
 @route('/static/chats-sidebar.css')
@@ -13,11 +13,11 @@ def files_sidebar_styles():
 	return static_file("files-sidebar.css", root='./static')
 
 @route('/static/favorites.css')
-def files_sidebar_styles():
+def favorites_styles():
 	return static_file("favorites.css", root='./static')
 
 @route('/static/add-favorites.css')
-def files_sidebar_styles():
+def add_favorites_styles():
 	return static_file("add-favorites.css", root='./static')
 
 @route('/')
@@ -25,7 +25,7 @@ def hello():
 	return template('index.html')
 
 @route('/chats-sidebar')
-def files_sidebar():
+def chats_sidebar():
 	return template('views/chats-sidebar.html')
 
 @route('/files-sidebar')
@@ -33,11 +33,15 @@ def files_sidebar():
 	return template('views/files-sidebar.html')
 
 @route('/favorites')
-def files_sidebar():
+def view_favorites():
 	return template('views/favorites.html')
 
 @route('/add-favorites')
-def files_sidebar():
+def add_favorites():
 	return template('views/add-favorites.html')
+
+@route('/play-video')
+def play_video():
+	return template('views/video.html')
 
 run(host='localhost', port=8080, debug=True)

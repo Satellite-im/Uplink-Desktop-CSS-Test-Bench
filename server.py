2fefdde7c7e9a6f2b1aac02d853433dab57b6b44
Stuart Woodbury <stuartwoodbury@protonmail.ch>
2022-11-22 14:09:19 -0500

    feat: added tests for webrtc

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 from bottle import route, run, static_file, template
 
+@route('/test/bbb') 
+def test_bbb():
+	return static_file("bbb_sunflower_1080p_30fps_stereo_arcd.mp4", root='/home/fu/Documents')
+
 @route('/static/tools.css')
 def tools_styles():
 	return static_file("tools.css", root='./static')
@@ -19,6 +23,10 @@ def favorites_styles():
 @route('/static/add-favorites.css')
 def add_favorites_styles():
 	return static_file("add-favorites.css", root='./static')
+
+@route('/static/webrtc.js')
+def webrtc_js():
+	return static_file("webrtc.js", root="./static")
 
 @route('/')
 def hello():
@@ -43,5 +51,13 @@ def add_favorites():
 @route('/play-video')
 def play_video():
 	return template('views/video.html')
+
+@route('/gst-sendrcv-demo')
+def gst_sendrcv_demo():
+	return template("views/gst-sendrcv-demo.html")
+
+@route('/webrtc-test1')
+def webrtc_test1():
+	return template("views/webrtc-test1.html")
 
 run(host='localhost', port=8080, debug=True)
